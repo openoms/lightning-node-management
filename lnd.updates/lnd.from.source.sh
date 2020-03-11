a7f0140e78407078444cb4507d25fd34afe96a28
openoms <oms@tuta.io>
2020-03-11 21:11:25 +0000

    lnd.update:formatting and remove all old binaries

--- lnd.updates/lnd.from.source.sh
+++ lnd.updates/lnd.from.source.sh
@@ -8,37 +8,51 @@
 repo="github.com/lightningnetwork/lnd"
 echo "Paste the latest or desired commit ID to checkout from"
 echo "See the list at: https://github.com/lightningnetwork/lnd/commits/master"
-echo "Example:"
-echo "4068e78af690f9b4a598de1f3f0b21b5560dd146"
+echo "Example (for v0.9.1 arm patch):"
+echo "b96de11678f6395b2e7d523af8ec4129e0d21df2"
 echo "(if left empty will use the latest state of the master branch)"
 echo "and press ENTER"
 read commit
-# commit="580509191007617afa6da4b6b0151b4b5313eb72"
-
+# commit="b96de11678f6395b2e7d523af8ec4129e0d21df2" # for v0.9.1 patch
+echo ""
 # BUILDING LND FROM SOURCE
-echo "*** Build LND from Source ***"
 echo "repo=${repo}"
 echo "up to the commit=${commit}"
-
+echo ""
+# stop service
 sudo systemctl stop lnd
 
+# make sure PATHs are defined
 export GOROOT=/usr/local/go
 export PATH=$PATH:$GOROOT/bin
 export GOPATH=/usr/local/gocode
 export PATH=$PATH:$GOPATH/bin
-echo "Deleting old source..."
-sudo rm -r /usr/local/gocode/src/github.com/lightningnetwork/lnd
+
+echo "Deleting old source"
+sudo rm -rf /usr/local/gocode/src/github.com/lightningnetwork/lnd
+echo ""
+
+echo "Downloading $repo"
 go get -d $repo
+cd $GOPATH/src/${repo}
+git checkout ${commit}
+echo ""
+
+echo "Deleting old binaries"
+sudo rm -f /usr/local/gocode/bin/lnd
+sudo rm -f /usr/local/bin/lnd
+sudo rm -f /usr/local/gocode/bin/lncli
+sudo rm -f /usr/local/bin/lncli
+echo ""
 
 echo "Building LND..."
-cd $GOPATH/src/${repo}
-sudo git checkout ${commit}
 make && make install
-sudo chmod 555 /usr/local/gocode/bin/lncli
-sudo chmod 555 /usr/local/gocode/bin/lnd
+sudo chmod 555 /usr/local/gocode/bin/lncli 2>/dev/null
+sudo chmod 555 /usr/local/gocode/bin/lnd 2>/dev/null
 sudo bash -c "echo 'export PATH=$PATH:/usr/local/gocode/bin/' >> /home/admin/.bashrc"
 sudo bash -c "echo 'export PATH=$PATH:/usr/local/gocode/bin/' >> /home/pi/.bashrc"
 sudo bash -c "echo 'export PATH=$PATH:/usr/local/gocode/bin/' >> /home/bitcoin/.bashrc"
+echo ""
 
 lndVersionCheck=$(lncli --version)
 if [ ${#lndVersionCheck} -eq 0 ]; then
@@ -47,11 +61,16 @@ if [ ${#lndVersionCheck} -eq 0 ]; then
   exit 1
 fi
 echo ""
-echo "** Link to /usr/local/bin ***"
-sudo ln -s /usr/local/gocode/bin/lncli /usr/local/bin/lncli
-sudo ln -s /usr/local/gocode/bin/lnd /usr/local/bin/lnd
+echo "Linking to /usr/local/bin"
+sudo ln -s /usr/local/gocode/bin/lncli /usr/local/bin/lncli 2>/dev/null
+sudo ln -s /usr/local/gocode/bin/lnd /usr/local/bin/lnd 2>/dev/null
 
+# start service
 sudo systemctl restart lnd
 
 echo ""
 echo "LND VERSION INSTALLED: ${lndVersionCheck} up to commit ${commit} from ${repo}"
+
+sleep 20
+echo "Unlock wallet - run 'lncli unlock' if fails"
+lncli unlock
